8971ea408c227c73d46cb8850e00485207996102
Andre Santos <andrefelipe_santos@outlook.com>
2023-10-23 00:14:28 -0300

    Refatora

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
+from random import randrange
+
 M1 = [1] # Determinante = 1
 M2 = [[2, 3], [1, 4]] # Determinante = 5
 M3 = [[3, 1, 0], [-2, 2, 3], [0, 1, 2]] # Determinante = 7
+
+def set_matrix(n: int):
+    return [[randrange(5) for coloumn in range(n)] for line in range(n)]
 
 def det(matrix: list):
     if len(matrix) == 1:
         return matrix[0]
     
     determinant = 0
-    line = matrix[0]
-    for colomn in range(len(line)):
-        determinant += line[colomn] * cof(matrix, colomn)
+    first_line = matrix[0]
+    for colomn in range(len(first_line)):
+        determinant += first_line[colomn] * cof(matrix, colomn)
     
     return determinant
 
@@ -19,20 +24,17 @@ def cof(matrix: list, index: int):
     return pow(-1, (i + j)) * det(minor(matrix, index))
 
 def minor(matrix: list, index: int):
-    new_matrix = []
-    for i in range(len(matrix)):
-        aux = []
-        for j in range(len(matrix[i])):
-            if i and j != index:
-                if len(matrix) > 2:
-                    aux.append(matrix[i][j])
-                    if aux not in new_matrix:
-                        new_matrix.append(aux)
-                else:
-                    new_matrix.append(matrix[i][j])
-
-    return new_matrix
+    if len(matrix) > 2:
+        return [[matrix[line][colomn] for colomn in range(len(matrix[line])) if colomn != index] for line in range(len(matrix)) if line != 0]
+    return [[matrix[line][colomn] for colomn in range(len(matrix[line])) if colomn != index][0] for line in range(len(matrix)) if line != 0]
     
 print(det(M1))
 print(det(M2))
 print(det(M3))
+
+M4 = set_matrix(2)
+print(M4, det(M4))
+M5 = set_matrix(3)
+print(M5, det(M5))
+M6 = set_matrix(4)
+print(M6, det(M6))
