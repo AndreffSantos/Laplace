bd3e76422ece6a9d78db97f2b71abe7fc20e0fa7
Andre Santos <andrefelipe_santos@outlook.com>
2023-10-22 23:31:54 -0300

    Feat: Implementa determinante de matriz de ordem 3

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 M1 = [1] # Determinante = 1
 M2 = [[2, 3], [1, 4]] # Determinante = 5
+M3 = [[3, 1, 0], [-2, 2, 3], [0, 1, 2]] # Determinante = 7
 
 def det(matrix: list):
     if len(matrix) == 1:
@@ -20,11 +21,18 @@ def cof(matrix: list, index: int):
 def minor(matrix: list, index: int):
     new_matrix = []
     for i in range(len(matrix)):
+        aux = []
         for j in range(len(matrix[i])):
             if i and j != index:
-                new_matrix.append(matrix[i][j])
+                if len(matrix) > 2:
+                    aux.append(matrix[i][j])
+                    if aux not in new_matrix:
+                        new_matrix.append(aux)
+                else:
+                    new_matrix.append(matrix[i][j])
 
     return new_matrix
     
 print(det(M1))
 print(det(M2))
+print(det(M3))
